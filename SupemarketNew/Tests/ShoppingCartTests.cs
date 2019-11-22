28c078c77827a90c46a2cbcacca714e67d2fed0d
Mr.White <kobilabok@users.noreply.github.com>
2019-11-22 14:32:21 -0600

    Added the Test folder

---
+++ SupemarketNew/Tests/ShoppingCartTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SupemarketNew.Tests
+{
+    [TestClass]
+    public class ShoppingCartTests
+    {
+        [TestMethod]
+        public void ShoppingCart_Customer_GetsAssigned_Test()
+        {
+            //Arrange
+            var shoppingCart = new ShoppingCart("Loyal");
+            var expectedCustomerName = "Loyal";
+
+            //Act
+            var actual = shoppingCart.CustomerName;
+
+            //Assert
+            Assert.AreEqual(expectedCustomerName, actual);
+        }
+    }
+}
